d05d285d041ce4821fc75881bb2254dde993c507
Gideon Spierings <gideongideon@gmail.com>
2021-08-29 18:16:26 +0200

    Fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("renders 'demo application' text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/demo application/i);
   expect(linkElement).toBeInTheDocument();
 });
